67cb3d9ed61ed73cbd64d63a5755c9af78f4d35e
Maksim <maksimgubenin@mail.ru>
2025-08-10 18:19:29 +0000

    Feat: Add templates.
    - Add configuration to usage base templates and base static;
    - Add templates for products app.

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -26,4 +28,4 @@ urlpatterns = [
     path("products/", include("products.urls")),
     path("customers/", include("customers.urls")),
     path("contracts/", include("contracts.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
