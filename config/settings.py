0c468b02a825c86365fbfa06a0db5d154fa5c9f2
Maksim <maksimgubenin@mail.ru>
2025-08-10 18:16:21 +0000

    chore(settings): Change settings configuration.
    - ALLOWED_HOSTS;
    - Upload python-dotenv;
    - Environment usage.

--- config/settings.py
+++ config/settings.py
@@ -10,12 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+import sys
 from os import getenv
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -28,7 +32,7 @@ SECRET_KEY = getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv("DJANGO_DEBUG", "0") == "1"
 
-ALLOWED_HOSTS: list[str] = []
+ALLOWED_HOSTS = getenv("ALLOWED_HOSTS", "127.0.0.1, localhost").split(",")
 
 
 # Application definition
