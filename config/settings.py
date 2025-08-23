15732d1fb23372caa287a6d1966881f5440ae6a8
Maksim <maksimgubenin@mail.ru>
2025-08-23 14:52:07 +0000

    Feat(product): i10n fix, l10n add

--- config/settings.py
+++ config/settings.py
@@ -141,6 +141,7 @@ LANGUAGES = [
 LOCALE_PATHS = [
     os.path.join(BASE_DIR, "templates", "locale"),
     os.path.join(BASE_DIR, "products", "locale"),
+    os.path.join(BASE_DIR, "advertisements", "locale"),
 ]
 
 TIME_ZONE = "UTC"
