b97da8d85ded59376ea7952a8058d07528af6856
Maksim <maksimgubenin@mail.ru>
2025-09-02 18:54:10 +0000

    Fix: workflow actions.

--- config/settings.py
+++ config/settings.py
@@ -26,9 +26,12 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = getenv(
-    "DJANGO_SECRET_KEY",
-)
+if "test" in sys.argv:
+    SECRET_KEY = "test-secret-key-for-ci-12345"
+else:
+    SECRET_KEY = getenv(
+        "DJANGO_SECRET_KEY", "django-insecure-fallback-key-for-development"
+    )
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv("DJANGO_DEBUG", "0") == "1"
